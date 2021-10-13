c5d7842104b5a8d03e7132703c7df863ddb85206
Jarret Nachtigal <jarretn1224@gmail.com>
2021-10-13 12:37:33 -0400

    created product model/db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "q", price: 1, image_url: "img", description: "r")

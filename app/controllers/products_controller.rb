4971e383583392a68079edcb566155f63493cec7
Jarret Nachtigal <jarretn1224@gmail.com>
2021-10-15 14:17:28 -0400

    added restful actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,52 @@
 class ProductsController < ApplicationController
-  def display_products
+  def index
     products = Product.all
     render json: products.as_json
   end
-  def display_first
-    products = Product.first
-    render json: products.as_json
+
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"]
+    )
+    product.save
+    render json: product.as_json
   end
+
+  def show
+    product = Product.find(params[:id])
+    render json: product.as_json
+  end
+
+  def update
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
+    product.name = params["name"] || product.name
+    product.price = params["price"]|| product.price
+    product.image_url = params["image_url"]|| product.image_url
+    product.description = params["description"]|| product.description
+
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    product_id = params["id"]
+    Product.destroy_by(id: product_id)
+    render json: { message: "destroyed"}
+  end
+  # def display_first
+  #   products = Product.first
+  #   render json: products.as_json
+  # end
+  # def product_wildcard # product_wildcard/3
+  #   product = params["product_id"]
+  #   render json: Product.find(product)
+  # end
+  # def product_query # ?product_id=#
+  #   product = params["product_id"]
+  #   render json: Product.find(product)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/products", controller: "products", action: "display_products"
-  get "/first", controller: "products", action: "display_first"
+  get "/products", controller: "products", action: "index"
+  post "/products", controller: "products", action: "create"
+  get "/products/:id", controller: "products", action: "show"
+  patch "/products/:id", controller: "products", action: "update"
+  delete "/products/:id", controller: "products", action: "destroy"
+
+  # get "/first", controller: "products", action: "display_first"
+  # get "/product_query", controller: "products", action: "product_query"
 end
